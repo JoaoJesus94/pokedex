02cb185aae0c7a684af0671495007b8ed4c96c74
JoaoJesus94 <joaojesus_94@hotmail.com>
2022-09-08 02:48:41 +0100

    feat: add pokemon type svg and svg loaader to next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: { domains: ['raw.githubusercontent.com'] },
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.tsx?$/,
+			use: ['@svgr/webpack'],
+		})
+
+		return config
+	},
 }
 
 module.exports = nextConfig
