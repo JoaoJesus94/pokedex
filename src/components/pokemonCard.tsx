108d6efd184d043b64fbcfbe95eaa31eb553c80d
João Jesus <47331628+JoaoJesus94@users.noreply.github.com>
2022-09-29 12:34:55 +0100

    fix: fix image optimization

--- src/components/pokemonCard.tsx
+++ src/components/pokemonCard.tsx
@@ -10,34 +10,35 @@ import { usePokemon } from '@/hooks/api/pokemon'
 import { NamedAPIResource } from '@/types/namedAPIResource'
 
 function Card({ pokemon }: { pokemon: NamedAPIResource }) {
-	const { data, isLoading } = usePokemon(pokemon.name)
+  const { data, isLoading } = usePokemon(pokemon.name)
 
-	if (isLoading) return <Loader />
+  if (isLoading) return <Loader />
 
-	if (!data) return null
+  if (!data) return null
 
-	return (
-		<div className="card bg-base-100 shadow-xl">
-			<div className="flex justify-center pt-2">
-				<Image
-					src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`}
-					width="150"
-					height="150"
-					alt={pokemon.name}
-				/>
-			</div>
-			<div className="card-body">
-				<div className="flex justify-between items-center">
-					<h2 className="card-title justify-between">{capitalize(pokemon.name)}</h2>
-					<div className="flex gap-3">
-						{data.types.map(({ type: { name } }) => (
-							<PokemonTypeIcon key={name} type={name} />
-						))}
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+  return (
+    <div className='card bg-base-100 shadow-xl'>
+      <div className='flex justify-center pt-2'>
+        <Image
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`}
+          unoptimized
+          width='150'
+          height='150'
+          alt={pokemon.name}
+        />
+      </div>
+      <div className='card-body'>
+        <div className='flex justify-between items-center'>
+          <h2 className='card-title justify-between'>{capitalize(pokemon.name)}</h2>
+          <div className='flex gap-3'>
+            {data.types.map(({ type: { name } }) => (
+              <PokemonTypeIcon key={name} type={name} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export const PokemonCard = memo(Card)
